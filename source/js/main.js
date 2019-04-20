285f40c602a280f50c560d5707ce4c0f9a1c1cd6
Sergei <kvakazuabr@uandex.ru>
2019-04-20 20:31:47 +0300

    fghjk

--- source/js/main.js
+++ source/js/main.js
@@ -47,3 +47,27 @@ myCollection.getMap().events.add('click', function() {
   }
 }
 startMap();*/
+
+
+var myMap;
+
+ymaps.ready(init);
+
+function init () {
+  myMap = new ymaps.Map("map", {
+    center: [59.938649906262164,30.323049135581975],
+    zoom: 17,
+    controls: [],
+    behaviors: ["drag"]
+  }),
+
+  myMap.controls.add(
+    new ymaps.control.ZoomControl()
+  );
+
+  var myPlacemark = new  ymaps.Placemark(
+    [59.938649906262164,30.323049135581975]);
+
+  myMap.geoObjects.add(myPlacemark),
+  myMap.controls.add("ZoomControl");
+}
